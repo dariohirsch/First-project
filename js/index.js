b88cfde7afb946db7fd8ea28ec2de4389b0efe12
dariohirsch <darohirsch@gmail.com>
2021-08-27 10:25:01 +0200

    boton de reset agregado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 window.onload = () => {
 	document.getElementById("button").onclick = () => {
 		const canvas = document.querySelector("#canvas");
+
 		app.init(canvas);
+		const button = document.querySelector("#button");
+		button.innerText = "Reset";
+		button.onclick = () => {
+			location.reload();
+		};
 	};
 };
